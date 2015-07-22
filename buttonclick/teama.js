8e9be9a61ca6c64070eca2ce2285a62a38dbae09
Shailesh <shabho@live.com>
2015-07-22 13:23:30 +0000

    namespace private public

---
+++ buttonclick/teama.js
@@ -0,0 +1,12 @@
+﻿var mindtree = mindtree || {};
+mindtree.teama = mindtree.teama || {};
+mindtree.teama.customer = function (firstname, lastname)
+{
+    this.firstname = firstname;    
+    this.lastname = lastname;
+    this.getfullname = function()
+    {
+        return this.firstname + "" + this.lastname;
+    }
+    return this;
+}
---
+++ buttonclick/teamb.js
@@ -0,0 +1,11 @@
+﻿var mindtree = mindtree || {};
+mindtree.teamb = mindtree.teamb || {};
+mindtree.teamb.customer = function (firstname, middlename, lastname) {
+    this.firstname = firstname;
+    this.secondname = middlename;
+    this.lastname = lastname;
+    this.getfullname = function () {
+        return this.firstname + "" + this.middlename + "" + this.lastname;
+    }
+    return this;
+}
