675139a6f95422be6cfc126b4560a42b0c744caa
Shailesh <shabho@live.com>
2015-07-23 06:03:29 +0000

    prototype and overriding

---
+++ buttonclick/employeescript.js
@@ -0,0 +1,9 @@
+﻿function employee(name) {
+    this.name = name;
+}
+//No matter how many objects we create functions are loaded only once in memory
+//Allows to override functions if required
+//we can overide built in function
+employee.prototype.getname = function () {
+    return this.name;
+}
